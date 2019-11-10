fde3c673baf7b8f5669edeff7f8154abd94b31f2
Ryan Kuba <ryankuba@gmail.com>
2019-11-09 20:46:09 -0800

    Initial commit

---
+++ settings.sh
@@ -0,0 +1,6 @@
+URL="http://releases.ubuntu.com/18.04/ubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
+TYPE=torrent or file
+CONTENTS="\
+casper/initrd|initrd
+casper/vmlinuz|vmlinuz
+casper/filesystem.squashfs|filesystem.squashfs"
