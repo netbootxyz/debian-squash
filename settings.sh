39d7e3419d57145f81514896d0d74f39e47a90c2
thelamer <ryankuba@gmail.com>
2019-11-14 03:48:28 -0800

    adding lxqt buster branch

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-REPLACE_VERSION-amd64-xfce.iso.torrent"
+URL="https://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-REPLACE_VERSION-amd64-lxqt.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 live/filesystem.squashfs|filesystem.squashfs"
