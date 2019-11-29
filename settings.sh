41d3900fab80bab5357457841b9644c0d3a15b31
thelamer <ryankuba@gmail.com>
2019-11-28 19:31:52 -0800

    adding tails 4.0 squash to assets

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-REPLACE_VERSION-amd64-standard.iso.torrent"
+URL="https://tails.boum.org/torrents/files/tails-amd64-4.0.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 live/filesystem.squashfs|filesystem.squashfs"
