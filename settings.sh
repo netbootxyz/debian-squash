7c9b03325c64c914b1b49324865f582a3e72cfd9
thelamer <ryankuba@gmail.com>
2019-12-21 12:19:42 -0800

    swap over to modifying ISO contents for asset ingestion

--- settings.sh
+++ settings.sh
@@ -1,4 +1,9 @@
 URL="https://downloads.sourceforge.net/project/q4os/stable/q4os-3.9-x64-tde.r1.iso"
 TYPE=file
 CONTENTS="\
-live/filesystem.squashfs|filesystem.squashfs"
+live/filesystem.squashfs|filesystem.squashfs
+live/initrd.img|initrd
+live/vmlinuz|vmlinuz"
+EXTRACT_INITRD="true"
+INITRD_NAME="initrd"
+INITRD_TYPE="gz"
