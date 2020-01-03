15a7415a65467bbcf7231fb015f19c9fba31e162
thelamer <ryankuba@gmail.com>
2020-01-03 14:51:46 -0800

    adding 3.10 assets for plasma

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://downloads.sourceforge.net/project/q4os/stable/q4os-3.9-x64.r1.iso"
+URL="https://downloads.sourceforge.net/project/q4os/stable/q4os-3.10-x64.r1.iso"
 TYPE=file
 CONTENTS="\
 live/filesystem.squashfs|filesystem.squashfs"
