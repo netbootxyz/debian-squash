0c7b8f469957ee43a19e1cc67d409c8f0f1e50b4
thelamer <ryankuba@gmail.com>
2020-01-16 15:54:25 -0800

    adding grml small assets

--- settings.sh
+++ settings.sh
@@ -1,9 +1,9 @@
-URL="http://download.grml.org/grml64-full_REPLACE_VERSION.iso"
+URL="http://download.grml.org/grml64-small_REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
-live/grml64-full/grml64-full.squashfs|filesystem.squashfs
-boot/grml64full/initrd.img|initrd
-boot/grml64full/vmlinuz|vmlinuz"
+live/grml64-small/grml64-small.squashfs|filesystem.squashfs
+boot/grml64small/initrd.img|initrd
+boot/grml64small/vmlinuz|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
-INITRD_TYPE="gz"
+INITRD_TYPE="xz"
