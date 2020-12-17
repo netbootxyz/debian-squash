f0daa4e4f98287dbd8c40116c70ed6248e4fcae2
netboot-ci <netboot-ci@netboot.xyz>
2020-12-17 06:34:52 +0000

    Updating workflow automation

---
+++ version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+VERSION=$(curl -sfL https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/MD5SUMS | awk -F '(debian-live-|-amd64-lxde)' '/-amd64-lxde/ {print $2;exit}')
+echo "${VERSION}"
