b9140face460dd65d943961d90752be37df65596
netboot-ci <netboot-ci@netboot.xyz>
2020-12-18 03:45:54 +0000

    Fix url

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://downloads.sourceforge.net/project/q4os/stable/q4os-3.10-x64-tde.r1.iso"
+URL="https://downloads.sourceforge.net/project/q4os/stable/q4os-3.12-x64-tde.r1.iso"
 TYPE=file
 CONTENTS="\
 live/filesystem.squashfs|filesystem.squashfs
