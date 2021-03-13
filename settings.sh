053dcfd01a0a0d8eecc4a2a44ee17cc63472700d
Antony Messerli <antony@mes.ser.li>
2021-03-13 10:51:39 -0600

    Add debian-default-10 live build

--- settings.sh
+++ settings.sh
@@ -1,4 +1,8 @@
-URL="https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/debian-live-REPLACE_VERSION-amd64-standard+nonfree.iso"
+URL="https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/debian-live-REPLACE_VERSION-amd64-xfce+nonfree.iso"
 TYPE=file
 CONTENTS="\
-live/filesystem.squashfs|filesystem.squashfs"
+live/initrd*|initrd
+live/vmlinuz*|vmlinuz"
+EXTRACT_INITRD="true"
+INITRD_NAME="initrd"
+INITRD_TYPE="gz"
