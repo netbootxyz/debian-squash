e2081f016a09f26721c532e0408af371348adaf7
Antony Messerli <antony@mes.ser.li>
2021-08-19 23:52:29 -0500

    Update Debian 11 core

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sfL https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/MD5SUMS | awk -F '(debian-live-|-amd64-standard)' '/-amd64-standard/ {print $2;exit}')
+VERSION=$(curl -sfL https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/SHA256SUMS | awk -F '(debian-live-|-amd64-standard)' '/-amd64-standard/ {print $2;exit}')
 echo "${VERSION}"
