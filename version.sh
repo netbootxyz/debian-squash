45415ce6ffd19c6dab1a70c9422841d8829bd3a8
Antony Messerli <antony@mes.ser.li>
2021-08-19 23:59:05 -0500

    Add Debian 11 KDE

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sfL https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/MD5SUMS | awk -F '(debian-live-|-amd64-kde)' '/-amd64-kde/ {print $2;exit}')
+VERSION=$(curl -sfL https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/SHA256SUMS | awk -F '(debian-live-|-amd64-kde)' '/-amd64-kde/ {print $2;exit}')
 echo "${VERSION}"
