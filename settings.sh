31153b653c07305463b9413b8099af534dfd57fc
Antony Messerli <antony@mes.ser.li>
2021-08-23 22:44:31 -0500

    Update voyager to bullseye

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://downloads.sourceforge.net/project/voyagerlive/Voyager-10.1-Debian-amd64.iso"
+URL="https://downloads.sourceforge.net/project/voyagerlive/Voyager-11-Debian-amd64.iso"
 TYPE=file
 CONTENTS="\
 live/filesystem.squashfs|filesystem.squashfs
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION="buster"
+VERSION="bullseye"
 echo "${VERSION}"
