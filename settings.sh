fdd40a1c1ce2d4a83db269414a0f801e829b344c
Antony Messerli <antony@mes.ser.li>
2021-08-27 01:08:58 -0500

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://downloads.vyos.io/rolling/current/amd64/vyos-1.3-rolling-REPLACE_VERSION-amd64.iso"
+URL="https://s3.amazonaws.com/s3-us.vyos.io/rolling/current/vyos-1.4-rolling-REPLACE_VERSION-amd64.iso"
 TYPE=file
 CONTENTS="\
 live/filesystem.squashfs|filesystem.squashfs
