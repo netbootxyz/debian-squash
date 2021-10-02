01c1b3c247536e29567654c9fef68103d807c476
Antony Messerli <antony@mes.ser.li>
2021-10-02 10:37:13 -0500

    Add clonezilla stable i686

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://clonezilla.org/downloads/stable/data/CHECKSUMS.TXT | awk -F '(clonezilla-live-|-amd64.iso)' '/clonezilla-live-/ {print $2;exit}')
+VERSION=$(curl -sL https://clonezilla.org/downloads/stable/data/CHECKSUMS.TXT | grep i686.iso | awk -F '(clonezilla-live-|-i686.iso)' '/clonezilla-live-/ {print $2;exit}')
 echo "${VERSION}"
