8fd60881624e187f848153f9024d841d34ae5139
Antony Messerli <antony@mes.ser.li>
2022-06-20 20:54:23 -0500

    Fix curl on version checker

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -s https://vyos.net/get/nightly-builds  | grep -i amd64.iso | head -n 1 | awk -F'-' {'print $5'})
+VERSION=$(curl -sL https://vyos.net/get/nightly-builds  | grep -i amd64.iso | head -n 1 | awk -F'-' {'print $5'})
 echo "${VERSION}"
