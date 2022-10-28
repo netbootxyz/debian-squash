175022e7253865d00b2e7df5b110acac442355a2
Antony Messerli <antony@mes.ser.li>
2022-10-28 00:41:28 -0500

    Update version checker

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://sparkylinux.org/download/rolling/ | grep x86_64-lxqt.iso | awk -F'-' '{print $3;exit}')
+VERSION=$(curl -sL https://sparkylinux.org/download/rolling/ | awk -F '(sparkylinux-|-x86_64-lxqt.iso)' '/-x86_64-lxqt.iso/ {print $2;exit}')
 echo "${VERSION}"
