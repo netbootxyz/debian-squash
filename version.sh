8b93d01a86efebab967436b7a4f3fe5f66574304
Antony Messerli <antony@mes.ser.li>
2023-02-16 18:44:02 -0600

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://deb.parrot.sh/direct//parrot/iso/5.1.2/signed-hashes.txt | grep "Parrot OS" | awk -F' ' '{print $3}')
+VERSION=$(curl -sL https://deb.parrot.sh/direct//parrot/iso/current/signed-hashes.txt | grep "Parrot OS" | awk -F' ' '{print $3}')
 echo "${VERSION}"
