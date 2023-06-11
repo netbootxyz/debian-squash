c6bfccaaf7b3c5e49c914dd632df2764845d8447
Antony Messerli <antony@mes.ser.li>
2023-06-11 17:43:09 -0500

    Debian 12 core

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sfL https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/SHA256SUMS | awk -F '(debian-live-|-amd64-standard)' '/-amd64-standard/ {print $2;exit}')
+VERSION=$(curl -sfL https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/SHA256SUMS | grep 'standard.iso' | grep -Po "(\d+\.)+\d+" | head -n1)
 echo "${VERSION}"
