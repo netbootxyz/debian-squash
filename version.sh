28c0b2815ee4e0419216b04d91eaff9f059e0d4d
Antony Messerli <antony@mes.ser.li>
2023-06-11 17:37:01 -0500

    Bump version

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -s https://www.deepin.org/en/download/ | awk -F '(deepin-desktop-community-|-amd64.iso)' '/deepin-desktop-community-/ {print $2;exit}')
+VERSION="20.9"
 echo "${VERSION}"
