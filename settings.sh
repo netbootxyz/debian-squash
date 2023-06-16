1b01d1d00434c2a8ca2100f6d50b4f016db45fbb
Antony Messerli <antony@mes.ser.li>
2023-06-15 23:08:16 -0500

    Add Deb 12

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/debian-live-REPLACE_VERSION-amd64-xfce+nonfree.iso"
+URL="https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-REPLACE_VERSION-amd64-xfce.iso"
 TYPE=file
 CONTENTS="\
 live/filesystem.squashfs|filesystem.squashfs"
