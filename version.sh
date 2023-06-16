c93405632571c815669c9a51743dfb7e092f0d7f
Antony Messerli <antony@mes.ser.li>
2023-06-15 22:45:32 -0500

    Add Deb 12

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sfL https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/SHA256SUMS | awk -F '(debian-live-|-amd64-gnome)' '/-amd64-gnome/ {print $2;exit}')
+VERSION=$(curl -sfL https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/SHA256SUMS | awk -F '(debian-live-|-amd64-gnome)' '/-amd64-gnome/ {print $2;exit}')
 echo "${VERSION}"
