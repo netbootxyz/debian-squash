31472c8744d1fbd5810567cc6a2d86786aa4ae4f
Antony Messerli <antony@mes.ser.li>
2024-12-23 11:48:44 -0600

    Add arm64 build

--- settings.sh
+++ settings.sh
@@ -1,9 +1,9 @@
-URL="https://download.grml.org/grml-small-REPLACE_VERSION-amd64.iso"
+URL="https://download.grml.org/grml-small-REPLACE_VERSION-arm64.iso"
 TYPE=file
 CONTENTS="\
-live/grml-small-amd64/grml-small-amd64.squashfs|filesystem.squashfs
-boot/grmlsmallamd64/initrd.img|initrd
-boot/grmlsmallamd64/vmlinuz|vmlinuz"
+live/grml-small-arm64/grml-small-arm64.squashfs|filesystem.squashfs
+boot/grmlsmallarm64/initrd.img|initrd
+boot/grmlsmallarm64/vmlinuz|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
 INITRD_TYPE="xz"
