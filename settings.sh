dd0246be024f9092c25f798661c5f53b19a7dac3
Antony Messerli <antony@mes.ser.li>
2024-12-23 09:38:54 -0600

    Update grml for updated arch links, add tag

--- settings.sh
+++ settings.sh
@@ -1,9 +1,9 @@
-URL="http://download.grml.org/grml64-full_REPLACE_VERSION.iso"
+URL="https://download.grml.org/grml-full-REPLACE_VERSION-amd64.iso"
 TYPE=file
 CONTENTS="\
-live/grml64-full/grml64-full.squashfs|filesystem.squashfs
-boot/grml64full/initrd.img|initrd
-boot/grml64full/vmlinuz|vmlinuz"
+live/grml-full-amd64/grml64-full.squashfs|filesystem.squashfs
+boot/grml-full-amd64/initrd.img|initrd
+boot/grml-full-amd64/vmlinuz|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
 INITRD_TYPE="xz"
