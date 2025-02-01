999ed7adde3a87f8d52ee9b72d9754c6af9e761c
Antony Messerli <antony@mes.ser.li>
2025-02-01 13:17:16 -0600

    Bump version

--- version.sh
+++ version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://deb.parrot.sh/direct//parrot/iso/current/signed-hashes.txt | grep "Parrot OS" | awk -F' ' '{print $3}')
+# current check broken, hopefully it comes back
+#VERSION=$(curl -sL https://deb.parrot.sh/direct//parrot/iso/current/signed-hashes.txt | grep "Parrot OS" | awk -F' ' '{print $3}')
+VERSION=$(curl -sL https://deb.parrot.sh/parrot/iso/6.3.2/signed-hashes.txt | grep "Parrot OS" | awk -F' ' '{print $3}')
 echo "${VERSION}"
